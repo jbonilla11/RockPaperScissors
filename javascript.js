767f524d17c3bd755699386bd7131af4830c447b
joaquin <jbonilla2814@gmail.com>
2022-08-27 18:50:37 -0500

    made DOM more

--- javascript.js
+++ javascript.js
@@ -1,67 +1,102 @@
 console.log("Rock, Paper, Scissors!")
 
-game()
+// Results stuff
 
-function game(){
-    let score = 0
+const container = document.querySelector('#container');
 
-    for (i = 0; i < 5; i++){
+const resulting = document.createElement('div');
+resulting.classList.add('div');
 
-    let userChoice = prompt("Rock, Paper, or Scissors?")
-    let computerChoice = getComputerChoice()
-    
-    let resultOriginal = playRound(userChoice, computerChoice)
-    let shortResult = resultOriginal.substring(4, 5)
-    
-    if (shortResult == "l"){
-        console.log("loss")
-        score--
-    } else if (shortResult == "w"){
-        console.log("win")
-        score++
-    } else {
-        console.log("tie")
-    }
-    }
+const scores = document.createElement('p');
+scores.classList.add('p');
 
-    console.log(score)
+const winner = document.createElement('h3');
+winner.classList.add('h3');
 
-}
+container.appendChild(resulting);
+container.appendChild(scores);
+container.appendChild(winner);
+
+// Some variables
+
+let computerChoice = "";
+let userChoice = "";
+let results = "";
+let userScore = 0;
+let computerScore = 0;
+
+// Button stuff
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    userChoice = "Rock";
+    computerChoice = getComputerChoice();
+    results = (playRound(userChoice, computerChoice));
+    resulting.textContent = results;
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    userChoice = "Paper";
+    computerChoice = getComputerChoice();
+    results = (playRound(userChoice, computerChoice));
+    resulting.textContent = results;
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    userChoice = "Scissors";
+    computerChoice = getComputerChoice();
+    results = (playRound(userChoice, computerChoice));
+    resulting.textContent = results;
+});
 
 function playRound(userChoice, computerChoice){
     let winnerLoser = "Error"
 
     if (userChoice == "Rock" && computerChoice == "Paper"){
-        winnerLoser = "You lose! Paper beats rock."
+        winnerLoser = "You lose! Paper beats rock.";
+        computerScore++;
     }  else if (userChoice == "Rock" && computerChoice == "Scissors"){
-        winnerLoser = "You win! Rock beats scissors."
+        winnerLoser = "You win! Rock beats scissors.";
+        userScore++;
     } else if (userChoice == "Paper" && computerChoice == "Rock"){
-        winnerLoser = "You win! Paper beats rock."
+        winnerLoser = "You win! Paper beats rock.";
+        userScore++;
     } else if (userChoice == "Paper" && computerChoice == "Scissors"){
-        winnerLoser = "You lose! Scissors beat paper."
+        winnerLoser = "You lose! Scissors beat paper.";
+        computerScore++;
     } else if (userChoice == "Scissors" && computerChoice == "Rock"){
-        winnerLoser = "You lose! Rock beats scissors."
+        winnerLoser = "You lose! Rock beats scissors.";
+        computerScore++;
     } else if (userChoice == "Scissors" && computerChoice == "Paper"){
-        winnerLoser = "You win! Scissors beat paper."
+        winnerLoser = "You win! Scissors beat paper.";
+        userScore++;
     } else {
-        winnerLoser = "It's a tie."
+        winnerLoser = "It's a tie.";
     }
 
-    return winnerLoser
+    scores.textContent = "you " + userScore + " - computer " + computerScore;
+    
+    if (userScore == 5){
+        winner.textContent = "Congrats! You win the game!";
+    } else if (computerScore == 5){
+        winner.textContent = "Oh no! You lose the game!";
+    }
+
+    return winnerLoser;
 }
 
 function getComputerChoice() {
-    let returnValue = "Error"
-    let numberGiven = Math.floor(Math.random() * 3)
+    let returnValue = "Error";
+    let numberGiven = Math.floor(Math.random() * 3);
 
     if (numberGiven == 0) {
-        returnValue = "Rock"
+        returnValue = "Rock";
     } else if (numberGiven == 1) {
-        returnValue = "Paper"
+        returnValue = "Paper";
     } else {
-        returnValue = "Scissors"
+        returnValue = "Scissors";
     }
-
-    console.log(returnValue)
     return returnValue;   
 }
